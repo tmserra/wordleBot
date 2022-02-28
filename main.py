d0296a88c5de9e9b93cb6cc01f06a3d869a74cce
tmserra <47332644+tmserra@users.noreply.github.com>
2022-02-28 18:18:40 +0100

    Código más limpio y documentado
    Documentadas las funciones importantes y eliminación de partes no usadas.

--- main.py
+++ main.py
@@ -3,28 +3,52 @@ import matplotlib
 import math
 """
 Spyder Editor
-
+@TODO: Se puede reducir el número de funciones que buscan palabras con X letras:
+    https://www.w3schools.com/python/python_functions.asp
+    apartado Arbitrary Keyword Arguments, **kwargs
 This is a temporary script file.
 """
 #counting the letters
 
-class WordClass:
-    def __init__(self, var1, var2):
-        self.var1 = var1
-        self.var2 = var2   
-    
+##
+#función que cuenta la cantidad de letras que hay en una lista de palabras.
+#atributos:
+    #string es la lista de strings que contiene las palabras
+    #letter es la letra que queremos contar
+#return:
+    #número(int) de veces que aparece una letra en el string
+##
 def count_letter(string, letter):
     contador = 0
     for word in string:
         contador = contador + word.count(letter)
     return contador
 
+##
+#función que convierte un string en una lista de strings separados por el espacio eliminando el espacio
+#atributos:
+    #string: string del que queremos obtener las palabras
+#return
+    #lista con las palabras de 5 letras separadas
+#TODO:
+    #está diseñado para palabras de 5 letras, quizás se podría parametrizar o ajustar según se quiera
+##
 def sliceToArray(string):
     wordsArray = []
     for i in range(0 , len(string), 6):
         wordsArray.append(string[i:(i+5)])
     return wordsArray
 
+##
+#función que busca palabras con una letra específica
+#atributos:
+    #string: lista de la que queremos obtener las palabras
+    #letra: letra que queremos que contenga la lista objetivo
+#return
+    #lista con las palabras que incluyan la letra
+#TODO:
+    #
+##
 def search_word(letter, string):
     wordsWithLetter = []
     contador = 0
@@ -40,7 +64,16 @@ def search_word(letter, string):
             return 0
         contador = 0
     return wordsWithLetter          
-
+##
+#función que ordena una lista de listas con las letras más comunes y la cantidad de palabras en las que aparece
+#atributos:
+    #wordList: lista con palabras en las que se quiere contar aparición de letras
+    #letras1: string con el alfabeto para buscar las letras
+#return
+    #lista las letras y la cantidad de palabras en las que aparece
+#TODO:
+    #limpiar o clarificar ciertas cosas
+##
 def most_letter(wordsList, letras1):
     contador = 0
     #cosa = WordClass("A", 0)
@@ -55,15 +88,17 @@ def most_letter(wordsList, letras1):
     lista.sort(key = lambda x: x[1], reverse = True)
     return lista
 
-def wordSearch(lista):
-    newList = []
-    wordList = []
-    for i in range(10):
-        newList.append(lista[i][0])
-    wordList = search_word(newList[i], newWord)
-    for i in newList:
-       wordList = search_word(wordList)
-       
+##
+#función que busca palabras con dos letras específicas
+#atributos:
+    #wordList: lista de la que queremos obtener las palabras
+    #letra1: letra que queremos que contenga la lista objetivo
+    #letra2: segunda letra que queremos que contenga la lista objetivo
+#return
+    #lista con las palabras que incluyan las letras
+#TODO:
+    #
+##  
 def wordSearch2(wordsList, letra1, letra2):
     newList =[]
     newList = search_word(letra1, wordsList)
@@ -71,7 +106,18 @@ def wordSearch2(wordsList, letra1, letra2):
         return 0
     newList = search_word(letra2, newList)
     return newList
-
+##
+#función que busca palabras con tres letras específicas
+#atributos:
+    #wordList: lista de la que queremos obtener las palabras
+    #letra1: letra que queremos que contenga la lista objetivo
+    #letra2: segunda letra que queremos que contenga la lista objetivo
+    #letra3: tercera letra que queremos que contenga la lista objetivo
+#return
+    #lista con las palabras que incluyan las letras
+#TODO:
+    #
+##  
 def wordSearch3(wordsList, letra1, letra2, letra3):
     newList =[]
     newList1 = []
@@ -88,7 +134,19 @@ def wordSearch3(wordsList, letra1, letra2, letra3):
         print("No hay palabras con ", letra1, letra2, letra3)
         return 0
     return newList2
-
+##
+#función que busca palabras con dos letras específicas
+#atributos:
+    #wordList: lista de la que queremos obtener las palabras
+    #letra1: letra que queremos que contenga la lista objetivo
+    #letra2: segunda letra que queremos que contenga la lista objetivo
+    #letra3: tercera
+    #letra4: cuarta
+#return
+    #lista con las palabras que incluyan las letras
+#TODO:
+    #
+##  
 def wordSearch4(wordsList, letra1, letra2, letra3, letra4):
     newList =[]
     newList = search_word(letra1, wordsList)
@@ -97,9 +155,23 @@ def wordSearch4(wordsList, letra1, letra2, letra3, letra4):
     newList = search_word(letra4, newList)
     return newList
 
+##
+#función que busca palabras con una letra en una posición especifica
+#atributos:
+    #wordList: lista de la que queremos obtener las palabras
+    #letra1: letra que queremos que contenga la lista objetivo
+    #posicion: posicion del 1 al 5 en la que se encuentra la letra1
+#return
+    #lista con las palabras que incluyan la letra letra1 en la posición posicion
+#TODO:
+    #
+##  
 def wordSearchPosition(wordList, letra1, posicion):
     wordsWithLetter = []
     contador = 0
+    if (0>=posicion<5):
+        print("Introduzca una posición entre 1 y 5")
+        return
     for word in wordList:
         if (word[posicion - 1] == letra1):
             wordsWithLetter.append(word)
@@ -109,6 +181,7 @@ def wordSearchPosition(wordList, letra1, posicion):
         return 0
     return wordsWithLetter
 
+## NO TENGO NI IDEA QUE HACE ESTA FUNCION, CREO QUE LO MISMO QUE HACE ESTO SE PUEDE CONSEGUIR MÁS FACILMENTE CON search_word()
 def wordSearchNotPosition(wordList, letra1, posicion):
     wordsWithLetter = []
     contador = 0
@@ -123,7 +196,16 @@ def wordSearchNotPosition(wordList, letra1, posicion):
         print("No hay palabras con la letra" + letra1 + "en la posicion", posicion)
         return 0
     return wordsWithLetter
-
+##
+#función que busca palabras que no contenga una letra
+#atributos:
+    #wordList: lista de la que queremos obtener las palabras
+    #letra1: letra que no queremos que contenga la lista objetivo
+#return
+    #lista con las palabras que no incluyan la letra
+#TODO:
+    #
+##  
 def wordSearchNoLetter(wordList, letra1):
     wordsWithLetter = []
     contador = 0
@@ -140,6 +222,17 @@ def wordSearchNoLetter(wordList, letra1):
         contador = 0
     return wordsWithLetter
 
+##
+#función que calcula la probabilidad de que una letra esté en una posición de la palabra
+#atributos:
+    #wordList: lista de la que queremos obtener las palabras
+    #letra1: letra que queremos que contenga la lista objetivo
+    #letra2: segunda letra que queremos que contenga la lista objetivo
+#return
+    #lista con las palabras que incluyan las letras
+#TODO:
+    #
+##  
 def letterPosition(wordList, letra1):
     timesLetter = [0, 0, 0, 0, 0]
     cuentaLetra = count_letter(wordList, letra1)
@@ -151,12 +244,15 @@ def letterPosition(wordList, letra1):
         timesLetter[i] = timesLetter[i]/cuentaLetra
     return timesLetter
 
+##función en desarrollo, calculo de entropía y valor de información de las palabras
 def entropyLetter(wordList):
     entropy = 0
     letrasTotales = 5*10836
     for char in letras:
-        lettercount = count_letter(wordList, char)
-        probability = lettercount / letrasTotales
+        cuentaLetra = letterPosition(wordList, char)
+        for position in cuentaLetra:
+            lettercount = count_letter(wordList, char)
+            probability = lettercount / letrasTotales
         if probability==0: probability=1
         entropy = entropy + (probability*math.log2(1/probability))
     return entropy
